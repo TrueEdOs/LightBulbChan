0ec33adecac3737b4d5eb8393c788207fb2c5d55
EdOs <comp.disk2013@yandex.by>
2019-11-22 23:33:27 +0300

    init commit

---
+++ src/main/java/com/light/bulb/chan/lightbulbchan/LightbulbchanApplication.java
@@ -0,0 +1,13 @@
+package com.light.bulb.chan.lightbulbchan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightbulbchanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightbulbchanApplication.class, args);
+	}
+
+}
